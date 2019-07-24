b0c62f1e9ccc1ef68b107b48e8c2b32375211c85
Yemre <aybey77@gmail.com>
2019-07-24 13:02:55 +0300

    first 2 test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Is not validated when name is nill" do
+    user = User.new(name:"")
+    expect(user).not_to be_valid
+  end
+  it "Is not validated when name is only one letter" do
+    user = User.new(name:"a")
+    expect(user).not_to be_valid
+  end
+
+  it "Is validated when name is not nill" do
+    user = User.new(name:"Cenk")
+    expect(user).to be_valid
+  end
+
+  it "Is destroying related events along with the user" do
+    user = User.new(name:"Cenk")
+    user.events << Event.new(event_title:"TestEvent", date: Time.now)
+    delete user_path(@user)
+    expect(Event.first).to be_nil
+  end
 end
